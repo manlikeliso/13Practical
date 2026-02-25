1db8083b5a00956b8a434537dc46146eba6ee294
manlikeliso <ndlovulisolethu@gmail.com>
2026-02-25 12:58:43 +0000

    progress1

--- prac3.java
+++ prac3.java
@@ -1,57 +1,291 @@
-// Code is stored as 13template.java
 import java.lang.Math.*;   import java.io.*;   import java.text.*;
 
-public class timeMethods{
-public static int N = ....;
-public static void main(String args[]){
+import java.text.DecimalFormat;
 
-DecimalFormat twoD = new DecimalFormat("0.00");
-DecimalFormat fourD = new DecimalFormat("0.0000");
-DecimalFormat fiveD = new DecimalFormat("0.00000");
 
-long start, finish;
-double runTime = 0, runTime2 = 0, time;
-double totalTime = 0.0;
-int n = N;
-int repetition, repetitions = 30;
 
-   runTime = 0;
-   for(repetition = 0; repetition < repetitions; repetition++) {
-      start = System.currentTimeMillis();
-		
-      // call the procedures to time here:
-      linearsearch (...);
-      binarysearch (...);
-      // Figure out how to alter this guideline here,
-		
-      finish = System.currentTimeMillis();
-			
-      time = (double)(finish - start);
-      runTime += time;
-      runTime2 += (time*time); }
+public class timeMethods {
 
-   double aveRuntime = runTime/repetitions;
-   double stdDeviation = 
-      Math.sqrt(runTime2 - repetitions*aveRuntime*aveRuntime)/(repetitions-1);
 
-   System.out.printf("\n\n\fStatistics\n");
-   System.out.println("________________________________________________");
-   System.out.println("Total time   =           " + runTime/1000 + "s.");
-   System.out.println("Total time\u00b2  =           " + runTime2);
-   System.out.println("Average time =           " + fiveD.format(aveRuntime/1000)
-                     + "s. " + '\u00B1' + " " + fourD.format(stdDeviation) + "ms.");
-   System.out.println("Standard deviation =     " + fourD.format(stdDeviation));
-   System.out.println("n            =           " + n);
-   System.out.println("Average time / run =     " + fiveD.format(aveRuntime/n*1000) 
-                     + '\u00B5' + "s. "); 
 
-   System.out.println("Repetitions  =             " + repetitions);
-   System.out.println("________________________________________________");
-   System.out.println();
-   System.out.println(); }	} 
+    public static int N = 32654; // number of records
 
-static void oneofyourMethods(int n, 
-                       yourMethodParameter1,
-                       yourMethodParameter2, . . . ) {
-// The declarations and body of your method / s   
-// The final statement of this code.} }
+
+
+    // ------------------ Node Class ------------------
+
+    static class Node {
+
+        int key;
+
+        String data;
+
+
+
+        Node(int key, String data) {
+
+            this.key = key;
+
+            this.data = data;
+
+        }
+
+    }
+
+
+
+    // ------------------ Linear Search ------------------
+
+    static Node linearSearch(Node[] array, int key) {
+
+        for (int i = 0; i < array.length; i++) {
+
+            if (array[i].key == key) {
+
+                return array[i];
+
+            }
+
+        }
+
+        return null;
+
+    }
+
+
+
+    // ------------------ Binary Search ------------------
+
+    static Node binarySearch(Node[] array, int key) {
+
+        int low = 0;
+
+        int high = array.length - 1;
+
+
+
+        while (low <= high) {
+
+            int mid = (low + high) / 2;
+
+
+
+            if (array[mid].key == key)
+
+                return array[mid];
+
+            else if (array[mid].key < key)
+
+                low = mid + 1;
+
+            else
+
+                high = mid - 1;
+
+        }
+
+        return null;
+
+    }
+
+
+
+    // ------------------ Main Method ------------------
+
+    public static void main(String[] args) {
+
+
+
+        DecimalFormat fiveD = new DecimalFormat("0.00000");
+
+
+
+        Node[] array = new Node[N];
+
+
+
+        // ----------- Read File -----------
+
+        try {
+
+            BufferedReader br = new BufferedReader(
+
+                    new FileReader("ulysses-1.numbered"));
+
+
+
+            String line;
+
+            int index = 0;
+
+
+
+            while ((line = br.readLine()) != null && index < N) {
+
+                int key = Integer.parseInt(line.substring(0, 5));
+
+                String data = line.substring(6);
+
+                array[index] = new Node(key, data);
+
+                index++;
+
+            }
+
+
+
+            br.close();
+
+        } catch (IOException e) {
+
+            System.out.println("Error reading file.");
+
+            return;
+
+        }
+
+
+
+        int repetitions = 30;
+
+
+
+        double linearTotal = 0;
+
+        double linearTotalSq = 0;
+
+
+
+        double binaryTotal = 0;
+
+        double binaryTotalSq = 0;
+
+
+
+        // ----------- Experiment -----------
+
+        for (int i = 0; i < repetitions; i++) {
+
+
+
+            int randomKey = (int)(Math.random() * 32654) + 1;
+
+
+
+            // ---- Linear Timing ----
+
+            long start = System.nanoTime();
+
+            linearSearch(array, randomKey);
+
+            long finish = System.nanoTime();
+
+
+
+            double linearTime = finish - start;
+
+
+
+            linearTotal += linearTime;
+
+            linearTotalSq += linearTime * linearTime;
+
+
+
+            // ---- Binary Timing ----
+
+            start = System.nanoTime();
+
+            binarySearch(array, randomKey);
+
+            finish = System.nanoTime();
+
+
+
+            double binaryTime = finish - start;
+
+
+
+            binaryTotal += binaryTime;
+
+            binaryTotalSq += binaryTime * binaryTime;
+
+        }
+
+
+
+        // ----------- Statistics -----------
+
+
+
+        double linearAvg = linearTotal / repetitions;
+
+        double linearStd = Math.sqrt(
+
+                (linearTotalSq - repetitions * linearAvg * linearAvg)
+
+                        / (repetitions - 1)
+
+        );
+
+
+
+        double binaryAvg = binaryTotal / repetitions;
+
+        double binaryStd = Math.sqrt(
+
+                (binaryTotalSq - repetitions * binaryAvg * binaryAvg)
+
+                        / (repetitions - 1)
+
+        );
+
+
+
+        // Convert nanoseconds to milliseconds
+
+        linearAvg /= 1_000_000.0;
+
+        linearStd /= 1_000_000.0;
+
+        binaryAvg /= 1_000_000.0;
+
+        binaryStd /= 1_000_000.0;
+
+
+
+        // ----------- Output -----------
+
+
+
+        System.out.println("\nRESULTS");
+
+        System.out.println("-----------------------------------");
+
+
+
+        System.out.println("Linear Search:");
+
+        System.out.println("Average Time = " + fiveD.format(linearAvg) + " ms");
+
+        System.out.println("Std Deviation = " + fiveD.format(linearStd) + " ms");
+
+
+
+        System.out.println();
+
+
+
+        System.out.println("Binary Search:");
+
+        System.out.println("Average Time = " + fiveD.format(binaryAvg) + " ms");
+
+        System.out.println("Std Deviation = " + fiveD.format(binaryStd) + " ms");
+
+
+
+        System.out.println("-----------------------------------");
+
+    }
+
+}
